6c606b73579c5bab183aecf14ca77aa3bf218d4e
Ryan Choi <4576425+rmerk@users.noreply.github.com>
2024-12-14 06:46:37 -0600

    added support for more layout on desktop

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="flex flex-col" style="min-height: calc(100vh - 4rem)">
         <div class="relative flex grow flex-col justify-center bg-green-900 text-white">
-            <div class="absolute right-0 top-0 p-8">
+            <div class="absolute bottom-[30%] right-[5%] p-8">
                 <p class="max-w-xs text-sm">
                     A delightful treat that combines the comforting charm of freshly baked cookies with the rich, luxurious and mouthwatering fillings.
                 </p>
@@ -9,20 +9,20 @@
                     Get more details <i class="fas fa-arrow-right" />
                 </a>
             </div>
-            <div class="absolute bottom-0 left-0 p-8">
+            <div class="absolute bottom-[10%] left-[15%] p-8">
                 <div class="flex items-center">
-                    <div class="rounded-full border border-white p-2">
-                        <span class="text-xs">
-                            BEST SELLER
-                        </span>
-                    </div>
+                    <img
+                        src="/female-avatar.png"
+                        alt="Sourdough logo"
+                        class="size-32 rounded-full"
+                    >
                 </div>
             </div>
             <div class="hero-image">
                 <img
                     src="/sourdough-hero-image.webp"
                     alt="Stack of freshly bakes cookies with ice cream"
-                    class="h-auto max-w-4xl object-contain"
+                    class="h-auto max-w-5xl object-contain"
                 >
             </div>
         </div>
